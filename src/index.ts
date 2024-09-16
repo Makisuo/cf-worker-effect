294e0e56e9f273fcd0812f15eba20a19502b9bcc
Makisuo <31933546+Makisuo@users.noreply.github.com>
2024-09-16 18:44:45 +0200

    Init

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,14 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { HttpApp, HttpRouter, HttpServerResponse } from "@effect/platform"
+
+const router = HttpRouter.empty.pipe(
+	HttpRouter.get("/", HttpServerResponse.text("Hello withPreResponseHandler")),
+	HttpRouter.get("/foo", HttpServerResponse.text("Other Route")),
+)
+
+const handler = HttpApp.toWebHandler(router)
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		return handler(request)
 	},
-} satisfies ExportedHandler<Env>;
+} satisfies ExportedHandler<Env>
